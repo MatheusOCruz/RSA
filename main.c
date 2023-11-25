e9ecda1aa410abf068d31b3b997c4d82f97fb9f5
Matheus <matheuscruz500@gmail.com>
2023-11-25 16:18:21 -0300

    ultimo?

--- main.c
+++ main.c
@@ -21,59 +21,31 @@ typedef uint64_t u64;
 
 int Miller_Rabin_test(mpz_t prime_canditate){
     // n-1 = 2^k*r
-
     if (mpz_cmp_ui(prime_canditate, 2) == 0 | mpz_cmp_ui(prime_canditate, 3) == 0){
-        // numero nunca deve ser tao pequeno, mas e um caso possivel ne ent fazer oq
         return true;
     }
     mpz_t r, results, p_sub1;
-
     mpz_inits(results, p_sub1, NULL);
-
-    // provavel q possa remover (garantir q numero nao e par na criacao)
-    mpz_mod_ui(results,prime_canditate,2);
-    if (mpz_cmp_ui(results,0) == 0) {
-
-        mpz_clear(results);
-        mpz_clear(p_sub1);
-
-        return false; // numero e par
-    }
-
     mpz_sub_ui(p_sub1, prime_canditate, 1); // p-1
     mpz_init_set(r,p_sub1);
-
-
     int k = 0; // potencia de 2
-
     mpz_mod_ui(results, p_sub1, 2);
-
     while (mpz_cmp_ui(results, 0) == 0) { // while r mod 2 != 0
         k++;
         mpz_div_ui(r, r, 2);
         mpz_mod_ui(results, r, 2);
     }  // enquanto o numero for divisivel por 2
-
-
     // aqui temos k e r(p_sub1)
-
-    // a quantidade de 'a' escolhido seria um parametro de seguranca
-
-
     u8 a_gen[128];
     mpz_t a;
     mpz_init(a);
-
     for (int i = 0; i <40; ++i) {
         randombytes(a_gen,128);
-        mpz_import(a,128,1,1,0,0,a_gen); // converte para o tipo magico
-        // a entre 2 e p-2 -> (a mod p-3) + 2
-        mpz_sub_ui (results,prime_canditate,3);
+        mpz_import(a,128,1,1,0,0,a_gen);
+        mpz_sub_ui (results,prime_canditate,3); // a entre 2 e p-2 -> (a mod p-3) + 2
         mpz_mod(a,a,results);
         mpz_add_ui(a,a,2);  // a entre 2 e p-2
-
         mpz_powm(results,a,r,prime_canditate);  // a^r mod p
-
         if (mpz_cmp_ui(results,1) == 0) return 1; // a^p-1 mod n == 1
     // testar para 0 a n-1
         for (u64 j = 0; j < k; ++j) {
@@ -94,7 +66,6 @@ int Miller_Rabin_test(mpz_t prime_canditate){
                 mpz_clear(p_sub1);
                 mpz_clear(r);
                 mpz_clear(a);
-
                 return false;
             }
         }
@@ -106,9 +77,8 @@ int Miller_Rabin_test(mpz_t prime_canditate){
     mpz_clear(p_sub1);
     mpz_clear(r);
     mpz_clear(a);
-
-    return 1;
-} // teoricamente funciona ??
+    return true;
+}
 
 void gen_prime(mpz_t prime) {
     u8 gen_buffer[128];
@@ -160,11 +130,37 @@ void RSA_init_keys(mpz_t e, mpz_t d, mpz_t n){
  * gen PS
  * pHash = Hash(P)
  */
-// funcoes auxiliares do treco
+
+
 void sha3(u8 *message, u8 *digest){
     const EVP_MD *md = EVP_sha3_512();
     unsigned int hash_len;
     EVP_Digest(message, sizeof(message), digest, &hash_len, md, NULL);
+}
+
+void sha3_file(char *file_path, u8 *digest){
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+    unsigned char buffer[BUFSIZE];
+    size_t bytes_read;
+    FILE *file;
+
+    md = EVP_sha3_512();
+    file = fopen(file_path, "rb");
+
+    if(!file) {
+        fprintf(stderr, "Error opening file\n");
+        return;
+    }
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    while((bytes_read = fread(buffer, 1, BUFSIZE, file)) != 0)
+        EVP_DigestUpdate(mdctx, buffer, bytes_read);
+
+    EVP_DigestFinal_ex(mdctx, digest, NULL);
+    EVP_MD_CTX_free(mdctx);
+
+    fclose(file);
 }
 
 void MGF(u8* seed, u8* mask,int len, int seedLen){
@@ -191,44 +187,37 @@ void MGF(u8* seed, u8* mask,int len, int seedLen){
 
 }
 
-// talvez o parametro seja a chave do aes
-// e a mensagem seja o trem criptografado pelo aes ???
-// num seeeei!
-
 
 //#################################################
-
-// teoricamente o oaep ta pronto
 
 void OAEP_encode(const char* M, char* EM, u8* Parameter, int emLen){
     size_t mLen = strlen(M);
     const size_t hash_len = 64; //sha-3 512
     size_t PS_len = emLen - (mLen + 2*hash_len + 1);
-
     // P pode ser de qualquer tamanho ja q sha-3 n tem problema com tamanho de input
     if (mLen > (emLen - 2*hash_len - 1) || PS_len <= 0 ) {
         printf("mensagem muito longa\n");
         return;
     }
     u8 PS[PS_len]; memset(PS,0,PS_len); //padding
-    u8 Phash[hash_len];
+    u8 Phash[hash_len]; //hash do parametro de seguranca
     sha3(Parameter, Phash);
     u8 DB[hash_len + PS_len + 1 + mLen]; // phash || PS ||01 || M
     memcpy(DB, Phash, hash_len);
     memcpy(DB + hash_len, PS, PS_len);
-    DB[hash_len+PS_len] = 0x01;
+    DB[hash_len+PS_len] = 0x01; // separador do padding
     memcpy(DB+hash_len+PS_len+1, M, mLen);
-    //gera seed
-    u8 seed[hash_len]; randombytes(seed, hash_len);
+
+    u8 seed[hash_len]; randombytes(seed, hash_len); //gera seed
     //gera DB = DB masked
-    u8 dbMask[emLen - hash_len]; MGF(seed, dbMask, emLen - hash_len, hash_len); // Db mask
+    u8 dbMask[emLen - hash_len];
+    MGF(seed, dbMask, emLen - hash_len, hash_len); // Db mask
     for (int i = 0; i < (emLen - hash_len); ++i) DB[i]^=dbMask[i]; // DB = masked DB
     u8 seedMask[hash_len]; MGF(DB, seedMask, hash_len, hash_len); // seed mask
     for (int i = 0; i < hash_len; ++i) seed[i] ^= seedMask[i]; // seed = masked seed
     // EM = masked seed || masked DB
     memcpy(EM,seed, hash_len);
     memcpy(EM+hash_len, DB, emLen - hash_len);
-
 
 }
 
@@ -241,11 +230,10 @@ u8* RSA_OAEP_encrypt(char *message ,mpz_t n, mpz_t e, u8* parameter, size_t *cou
     mpz_powm(c,c,e,n);
     return (u8 *) mpz_export(NULL,count, 1,1,0,0,c);
 
-} // pronta?
+}
 
 // ###############################################
 
-//agora pra tentar o decrypt dessa joca
 // len em >= 2hlen+1 -> 123
 
 
@@ -254,33 +242,24 @@ u8* OAEP_decode(const char *EM,  u8 *Parameter){
     const size_t hash_len = 64; //sha-3 512
     size_t db_len = emLen-hash_len;
     if (emLen <= hash_len + 1) printf("decoding error: size\n");
-
     u8 maskedSeed[hash_len], maskedDB[db_len];
     memcpy(maskedSeed,EM, hash_len);
     memcpy(maskedDB,EM+hash_len, emLen-hash_len);
-
     u8 seedMask[hash_len], dbMask[db_len];
-    // restaura seed
     MGF(maskedDB,seedMask,hash_len, db_len);
-
-    for (int i = 0; i < hash_len; ++i) maskedSeed[i] ^= seedMask[i];
+    for (int i = 0; i < hash_len; ++i) maskedSeed[i] ^= seedMask[i]; // restaura a seed
     // restaura DB
     MGF(maskedSeed, dbMask,db_len, hash_len);
-    for (int i = 0; i < db_len; ++i) maskedDB[i] ^= dbMask[i];
-
+    for (int i = 0; i < db_len; ++i) maskedDB[i] ^= dbMask[i];     // restaura DB
     u8 Phash[hash_len]; sha3(Parameter, Phash);
-
     // DB PARTS phash || PS ||01 || M
-
     // checa de se o Phash ta igual
     for (int i = 0; i < hash_len; ++i) {
         if (maskedDB[i] != Phash[i] ){
             printf("decoding error: Phash\n ");
             return NULL;
         }
-    }
-
-    //mesmo phash, agr e pra achar o comeco da msg
+    }  //mesmo phash, agr e pra achar o comeco da msg
     int m_start = 0;
     for (int i = hash_len; i < db_len; ++i) {
         if (maskedDB[i] == 0x01) {
@@ -295,7 +274,6 @@ u8* OAEP_decode(const char *EM,  u8 *Parameter){
     size_t mLen = db_len - m_start;
     u8 *message = malloc(mLen);
     memcpy(message, maskedDB+m_start, mLen);
-
     return message;
 }
 
@@ -315,34 +293,11 @@ u8* RSA_OAEP_decrypt(u8 *ciphertext, mpz_t n, mpz_t d, u8* Parameter, size_t cou
 }
 
 //################################################
-void hash_file_sha3(char *file_path, u8 *digest){
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
-    unsigned char buffer[BUFSIZE];
-    size_t bytes_read;
-    FILE *file;
 
-    md = EVP_sha3_512();
-    file = fopen(file_path, "rb");
-
-    if(!file) {
-        fprintf(stderr, "Error opening file\n");
-        return;
-    }
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
-    while((bytes_read = fread(buffer, 1, BUFSIZE, file)) != 0)
-        EVP_DigestUpdate(mdctx, buffer, bytes_read);
-
-    EVP_DigestFinal_ex(mdctx, digest, NULL);
-    EVP_MD_CTX_free(mdctx);
-
-    fclose(file);
-}
 
 
 //##################################################
-//bendita base64
+// base64
 
 
 char* BASE64_encode(char* input, size_t input_size, size_t *output_size){
@@ -403,10 +358,8 @@ char* BASE64_decode(char* input, size_t input_size, size_t *output_size){
     if (pad){
         int n = B64_decoder[input[len]] << 18 | B64_decoder[input[len + 1]] << 12;
         output[(int)*output_size - 1] = n >> 16;
-
         if (input_size > len + 2 && input[len + 2] != '='){
             n |= B64_decoder[input[len + 2]] << 6;
-            printf("%d",*output_size);
             output = (char*) realloc(output, (int)*output_size + 1);
             *output_size += 1;
             output[(int)*output_size -1] = n >> 8 & 0xff;
@@ -424,7 +377,7 @@ u8* RSA_sign(char *file_path, mpz_t d, mpz_t n){
     mpz_t t;
     mpz_init(t);
     u8 digest[64];
-    hash_file_sha3(file_path, digest);
+    sha3_file(file_path, digest);
     mpz_import(t,64,1,1,0,0,digest);
     // n sei oq seria o parametro ent na duvida vai uns 0
     u8 parameter[64];
@@ -457,7 +410,7 @@ int RSA_verify(char* file_path,u8* full_msg, mpz_t n, mpz_t e){
     memset(Parameter, 0, 64);
     u8 *received_hash = RSA_OAEP_decrypt(ciphertext, n, e, Parameter, ciphertext_len);
     u8 hash[64];
-    hash_file_sha3(file_path, hash);
+    sha3_file(file_path, hash);
     for (int i = 0; i < 64; ++i) {
         if (hash[i] != received_hash[i]){
             printf("tem uma cobra na minha bota!\n");
@@ -469,7 +422,6 @@ int RSA_verify(char* file_path,u8* full_msg, mpz_t n, mpz_t e){
     return true;
 }
 
-// na teoria isso aqui ja foi
 // ##################################################
 
 
@@ -486,25 +438,8 @@ int main() {
     mpz_t e,d,n;
     mpz_inits(e,d,n, NULL);
     RSA_init_keys(e, d, n);
-
-    // calculo de hash de mensagem em claro
-    /*u8 digest[64];
-    char* file_path = "/home/matheus/CLionProjects/RSA/teste.txt";
-    hash_file_sha3(file_path, digest);
-
-
-    size_t count;
-    u8 *aaa =  RSA_OAEP_encrypt("suco de fruta com tamaridno",  n, e, digest, &count);
-    printf("com rsa:");
-    printf("%x", aaa[0]);
-    for (int i = 1; i < count; ++i) {
-    printf("%02x", aaa[i]);
-    }
-printf("\n");
-
-RSA_OAEP_decrypt(aaa, n, d, digest, count);*/
-    char* file_path = "/home/matheus/CLionProjects/RSA/teste.txt";
-    u8* sign = RSA_sign(file_path, d, n);
+    char* file_path = "/home/matheus/CLionProjects/RSA/teste.txt"; //trocar pelo path do arquivo
+    u8* sign = RSA_sign(file_path, d, n); // assina com chave privada (d,n)
     if (RSA_verify(file_path, sign, n, e)) printf("um milagre de natal!\n");
     size_t l;
     char* a = BASE64_encode("eu gosto de suco de frutac",26, &l);
